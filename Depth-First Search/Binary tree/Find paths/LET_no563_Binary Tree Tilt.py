aa079fb20413d5f95393c06e2f4d9c2afaa4885f
Yi-Chang Lin <kiraskywing@gmail.com>
2020-11-09 10:04:23 +0800

    Add Leetcode practice 20201109

---
+++ Depth-First Search/Binary tree/Find paths/LET_no563_Binary Tree Tilt.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTilt(self, root: TreeNode) -> int:
+        res, _ = self.dfs(root)
+        return res
+    
+    def dfs(self, root):
+        if not root:
+            return 0, 0
+        left_diff, left_total = self.dfs(root.left)
+        right_diff, right_total = self.dfs(root.right)
+        return left_diff + right_diff + abs(left_total - right_total), left_total + right_total + root.val
