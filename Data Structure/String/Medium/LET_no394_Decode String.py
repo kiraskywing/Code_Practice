228498746f5b82b95cdb8675c2a33262a0483e4b
Yi-Chang Lin <kiraskywing@gmail.com>
2020-11-20 09:52:33 +0800

    Add Leetcode daily challenge 20201120

---
+++ Data Structure/String/Medium/LET_no394_Decode String.py
@@ -0,0 +1,18 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack, num, cur_str = [], 0, ''
+        
+        for ch in s:
+            if ch == '[':
+                stack.append([cur_str, num])
+                cur_str = ''
+                num = 0
+            elif ch == ']':
+                pre_str, pre_num = stack.pop()
+                cur_str = pre_str + pre_num * cur_str
+            elif ch.isdigit():
+                num = num * 10 + int(ch)
+            else:
+                cur_str += ch
+        
+        return cur_str
