8e6b68479c3f4a9f404177348a3a1cba777df4f0
Yi-Chang Lin <kiraskywing@gmail.com>
2020-12-18 10:41:06 +0800

    test 2

--- Data Structure/Hash Map/LET_no454_4Sum II.py
+++ Data Structure/Hash Map/LET_no454_4Sum II.py
@@ -2,4 +2,3 @@ class Solution:
     def fourSumCount(self, A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
         AB = collections.Counter(a + b for a in A for b in B)
         return sum(AB[-c - d] for c in C for d in D)
-            dd
