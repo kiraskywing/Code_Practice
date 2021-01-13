e4591d30d69a6d655a7748402cccb0dabfbc6c1e
Yi-Chang Lin <kiraskywing@gmail.com>
2021-01-13 09:48:50 +0800

    Add LeetCode daily challenge 20210113

--- Data Structure/Linked List/LNT_no167_Add Two Numbers.py
+++ Data Structure/Linked List/LNT_no167_Add Two Numbers.py
@@ -1,44 +1,28 @@
-"""
-Definition of ListNode
-class ListNode(object):
-    def __init__(self, val, next=None):
-        self.val = val
-        self.next = next
-"""
+# The same as LeetCode no2. Add Two Numbers
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    """
-    @param l1: the first list
-    @param l2: the second list
-    @return: the sum list of l1 and l2
-    """
-    def addLists(self, l1, l2):
-
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         dummy = ListNode(-1)
         cur = dummy
-        adder = False
-
-        while True:
-            val = 0
+        nxt_digit = 0
+        
+        while l1 or l2:
+            value = nxt_digit
             if l1:
-                val += l1.val
+                value += l1.val
                 l1 = l1.next
             if l2:
-                val += l2.val
+                value += l2.val
                 l2 = l2.next
-            if adder:
-                val += 1
-            cur.next = ListNode(val % 10)
+            cur.next = ListNode(value % 10)
+            nxt_digit = value // 10
             cur = cur.next
-
-            if val >= 10:
-                adder = True
-            else:
-                adder = False
-            if not l1 and not l2:
-                break
-
-        if adder:
-            cur.next = ListNode(1)
-
+        if nxt_digit:
+            cur.next = ListNode(nxt_digit)
+        
         return dummy.next
