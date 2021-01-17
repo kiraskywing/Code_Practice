cd14c6d8c99221b0d4a813efd450797ec335449b
Yi-Chang Lin <kiraskywing@gmail.com>
2021-01-17 20:35:52 +0800

    Add LeetCode daily challenge 20210118

--- Dynamic Programming/Medium/LET_no1641_Count Sorted Vowel Strings.py
+++ Dynamic Programming/Medium/LET_no1641_Count Sorted Vowel Strings.py
@@ -2,6 +2,7 @@ class Solution:
     def countVowelStrings(self, n: int) -> int:
         dp = [[1] * 6] + [[0] * 6 for _ in range(n)]
         
+        # dp[n][k] means the number of strings constructed by at most k different characters.
         for i in range(1, n + 1):
             for k in range(1, 6):
                 dp[i][k] = dp[i][k - 1] + dp[i - 1][k]
