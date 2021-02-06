229c1d0ad4a183f8b628d0bc1aa734f0835d7d6c
Yi-Chang Lin <kiraskywing@gmail.com>
2021-02-06 10:59:06 +0800

    Add LeetCode daily challenge 20210206

---
+++ Data Structure/String/Medium/LET_no71_Simplify Path.py
@@ -0,0 +1,11 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        res = []
+        for p in path.split('/'):
+            if p == '..':
+                if res:
+                    res.pop()
+            elif p and p != '.':
+                res.append(p)
+        
+        return '/' + '/'.join(res)
