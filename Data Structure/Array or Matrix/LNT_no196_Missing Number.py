65c76c876abe042ef3772cfee7b3edbff5015d17
Yi-Chang Lin <kiraskywing@gmail.com>
2021-03-03 22:48:55 +0800

    Add LeetCode daily challenge 20210304

--- Data Structure/Array or Matrix/LNT_no196_Missing Number.py
+++ Data Structure/Array or Matrix/LNT_no196_Missing Number.py
@@ -1,4 +1,15 @@
-class Solution:
+# The same as LeetCode no.268. Missing Number
+class Solution1:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = len(nums)
+        i = 0
+        for num in nums:
+            res ^= num
+            res ^= i
+            i += 1
+        return res
+
+class Solution2:
     """
     @param nums: An array of integers
     @return: An integer
