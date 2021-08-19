f12480b14ee7c5e4cc80dcea65b0961da6cfca34
Yi-Chang Lin <kiraskywing@gmail.com>
2021-08-19 09:04:52 +0800

    Add LeetCode daily challenge 20210819

--- Dynamic Programming/Medium/LET_no91_Decode Ways.py
+++ Dynamic Programming/Medium/LET_no91_Decode Ways.py
@@ -1,15 +1,11 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if not s:
-            return 0
-
-        dp = [1] + [0 for _ in range(len(s))]
-        dp[1] = 0 if s[0] == "0" else 1
-
-        for i in range(2, len(s) + 1): 
-            if 0 < int(s[i - 1 : i]) <= 9:
-                dp[i] += dp[i - 1]
-            if 10 <= int(s[i - 2: i]) <= 26:
-                dp[i] += dp[i - 2]
+        p, pp, n = 1, 0, len(s)
+        for i in range(n - 1, -1, -1):
+            cur = 0 if s[i] == '0' else p
+            if i < n - 1 and (s[i] == '1' or (s[i] == '2' and int(s[i + 1]) < 7)):
+                cur += pp
+            pp = p
+            p = cur
         
-        return dp[len(s)]
+        return 0 if len(s) == 0 else p
