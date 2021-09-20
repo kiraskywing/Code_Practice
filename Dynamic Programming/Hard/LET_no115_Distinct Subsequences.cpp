d59670c2c070b2d983059688e3c50fbb23163b69
Yi-Chang Lin <kiraskywing@gmail.com>
2021-09-20 09:52:59 +0800

    Add LeetCode daily challenge 20210920

---
+++ Dynamic Programming/Hard/LET_no115_Distinct Subsequences.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = t.size(), n = s.size();
+        vector<vector<unsigned long long>> dp(m + 1, vector<unsigned long long>(n + 1, 0));
+        for (int j = 0; j <= n; j++)
+            dp[0][j] = 1;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++)
+                dp[i][j] = dp[i][j - 1] + (s[j - 1] == t[i - 1] ? dp[i - 1][j - 1] : 0);
+        }
+        return dp[m][n];
+    }
+};
