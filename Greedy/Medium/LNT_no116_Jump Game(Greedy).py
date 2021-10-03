e926971639d3d7396bcb40a87b38e0835c30495b
Yi-Chang Lin <kiraskywing@gmail.com>
2021-10-03 09:08:37 +0800

    Add LeetCode daily challenge 20211003

---
+++ Greedy/Medium/LNT_no116_Jump Game(Greedy).py
@@ -0,0 +1,15 @@
+# The same as LeetCode no55.Jump Game
+
+class Solution:
+    """
+    @param A: A list of integers
+    @return: A boolean
+    """
+
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        i = reach = 0
+        while i < n and i <= reach:
+            reach = max(reach, i + nums[i])
+            i += 1
+        return i == n
