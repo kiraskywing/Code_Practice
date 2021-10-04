c1b6543647d2bb33b12653eed7195b1add96c0e5
Yi-Chang Lin <kiraskywing@gmail.com>
2021-10-04 09:57:05 +0800

    Add LeetCode daily challenge 20211004

---
+++ Breadth-First Search/Matrix pattern/LET_no463_Island Perimeter.py
@@ -0,0 +1,27 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        queue = collections.deque([])
+        m, n = len(grid), len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]:
+                    queue.append((i, j))
+                    grid[i][j] = -1
+                    break
+        
+        res = 0
+        while queue:
+            i, j = queue.popleft()
+            
+            for di, dj in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+                i2, j2 = i + di, j + dj
+                if not (0 <= i2 < m and 0 <= j2 < n):
+                    res += 1
+                else:
+                    if grid[i2][j2] == 0:
+                        res += 1
+                    elif grid[i2][j2] == 1:
+                        queue.append((i2, j2))
+                        grid[i2][j2] = -1
+        
+        return res
