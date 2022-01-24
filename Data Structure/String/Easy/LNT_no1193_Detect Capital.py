5e0432c33f36c1bf876e40842849cdfc8e90375f
Yi-Chang Lin <kiraskywing@gmail.com>
2022-01-24 09:11:39 +0800

    Add LeetCode daily challenge 20220124

---
+++ Data Structure/String/Easy/LNT_no1193_Detect Capital.py
@@ -0,0 +1,8 @@
+# The same as LeetCode no520. Detect Capital
+
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        caps = 0
+        for c in word:
+            caps += 'A' <= c <= 'Z'
+        return caps == len(word) or (caps == 1 and 'A' <= word[0] <= 'Z') or caps == 0
