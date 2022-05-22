2999e90e3996eff15b388f246ac8145c46842b1a
Yi-Chang Lin <kiraskywing@gmail.com>
2022-05-22 14:45:30 +0800

    update 20220522

--- Dynamic Programming/Medium/LET_no647_Palindromic Substrings.py
+++ Dynamic Programming/Medium/LET_no647_Palindromic Substrings.py
@@ -3,10 +3,11 @@ class Solution:
         n = len(s)
         dp = [[0] * n for _ in range(n)]
         res = 0
-        
-        for i in range(n - 1, -1, -1):
-            for j in range(i, n):
-                dp[i][j] = s[i] == s[j] and ((j - i + 1) < 3 or dp[i+1][j-1])
-                res += dp[i][j]
+            
+        for d in range(n):
+            for left in range(n - d):
+                right = left + d
+                dp[left][right] = s[left] == s[right] and (right - left < 3 or dp[left + 1][right - 1])
+                res += dp[left][right]
         
         return res
