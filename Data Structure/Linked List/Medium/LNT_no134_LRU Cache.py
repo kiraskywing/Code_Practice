753a228d8d2b0ac074dfba251fe60d82aaa1105b
Yi-Chang Lin <kiraskywing@gmail.com>
2022-06-02 13:00:14 +0800

    update 20220602

--- Data Structure/Linked List/Medium/LNT_no134_LRU Cache.py
+++ Data Structure/Linked List/Medium/LNT_no134_LRU Cache.py
@@ -1,9 +1,11 @@
 # The same as LeetCode no146. LRU Cache
 
 class Node:
-    def __init__(self, key, val):
-        self.key, self.val = key, val
-        self.prev, self.next = None, None
+    def __init__(self, key, val, prev=None, next=None):
+        self.key = key
+        self.val = val
+        self.prev = prev
+        self.next = next
 
 class LRUCache:
 
@@ -11,49 +13,51 @@ class LRUCache:
         self.capacity = capacity
         self.dummy = Node(-1, -1)
         self.tail = self.dummy
-        self.memo = {}
+        self.memo = dict()
 
     def get(self, key: int) -> int:
         if key not in self.memo:
             return -1
         cur = self.memo[key]
-        self.kick(cur)
+        self.kickToTail(cur)
         return cur.val
 
     def put(self, key: int, value: int) -> None:
         if key in self.memo:
             cur = self.memo[key]
-            self.kick(cur)
             cur.val = value
-            return
+            self.kickToTail(cur)
+        else:
+            cur = Node(key, value)
+            self.memo[key] = cur
+            self.addToTail(cur)
+            if self.capacity == 0:
+                self.popFront()
+            else:
+                self.capacity -= 1
+                
+    
+    def popFront(self) -> None:
+        pop_node = self.dummy.next
+        self.connectPrevNext(pop_node)
+        del self.memo[pop_node.key]
+    
+    def connectPrevNext(self, cur: Node) -> None:
+        prev_node = cur.prev
+        next_node = cur.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+    
+    def addToTail(self, node: Node) -> None:
+        self.tail.next = node
+        node.prev = self.tail
+        self.tail = node
         
-        cur = Node(key, value)
-        self.memo[key] = cur
-        self.pushToTail(cur)
-        if len(self.memo) > self.capacity:
-            self.popFront()
-    
-    def kick(self, cur):
-        if cur is self.tail:
+    def kickToTail(self, node: Node) -> None:
+        if node is self.tail:
             return
-        p, n = cur.prev, cur.next
-        p.next = n
-        n.prev = p
-        self.pushToTail(cur)
-    
-    def pushToTail(self, cur):
-        self.tail.next = cur
-        cur.prev = self.tail
-        cur.next = None
-        self.tail = cur
-    
-    def popFront(self):
-        head = self.dummy.next
-        nxt = head.next
-        del self.memo[head.key]
-        self.dummy.next = nxt
-        nxt.prev = self.dummy
-
+        self.connectPrevNext(node)
+        self.addToTail(node)
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
