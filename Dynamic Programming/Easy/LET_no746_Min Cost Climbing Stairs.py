fc4be4ea3f1b38aacb36b4161032ba1e8778dd9a
Yi-Chang Lin <kiraskywing@gmail.com>
2022-07-10 17:52:16 +0800

    update 20220710

--- Dynamic Programming/Easy/LET_no746_Min Cost Climbing Stairs.py
+++ Dynamic Programming/Easy/LET_no746_Min Cost Climbing Stairs.py
@@ -7,3 +7,10 @@ class Solution:
             else:
                 dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
         return min(dp[-1], dp[-2])
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost.append(0)
+        for i in range(2, len(cost)):
+            cost[i] += min(cost[i - 1], cost[i - 2])
+        return cost[-1]
