e87f4b790eb386a7452448f22d606af9075f1e24
Yi-Chang Lin <kiraskywing@gmail.com>
2022-07-12 15:59:56 +0800

    update 20220712

--- Dynamic Programming/Medium/LET_no322_Coin Change.py
+++ Dynamic Programming/Medium/LET_no322_Coin Change.py
@@ -2,7 +2,8 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [0] + [float('inf')] * amount
         
-        for i in range(1, amount + 1):
-            dp[i] = min(dp[i - c] + 1 if i >= c else float('inf') for c in coins)
+        for c in coins:
+            for i in range(c, amount + 1):
+                dp[i] = min(dp[i], dp[i - c] + 1)
         
-        return dp[-1] if dp[-1] != float('inf') else -1
+        return dp[amount] if dp[amount] != float('inf') else -1
