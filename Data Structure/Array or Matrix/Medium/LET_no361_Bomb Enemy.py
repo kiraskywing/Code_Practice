32ae869c517540b5095e84b00bb7ae62f3a49340
Yi-Chang Lin <kiraskywing@gmail.com>
2022-07-29 17:07:23 +0800

    update 20220729

--- Data Structure/Array or Matrix/Medium/LET_no361_Bomb Enemy.py
+++ Data Structure/Array or Matrix/Medium/LET_no361_Bomb Enemy.py
@@ -9,19 +9,17 @@ class Solution:
             for j in range(n):
                 if j == 0 or grid[i][j - 1] == 'W':
                     row_sum = 0
-                    for k in range(j, n):
-                        if grid[i][k] == 'W':
-                            break
-                        if grid[i][k] == 'E':
-                            row_sum += 1
+                    k = j
+                    while k < n and grid[i][k] != 'W':
+                        row_sum += grid[i][k] == 'E'
+                        k += 1
                 if i == 0 or grid[i - 1][j] == 'W':
                     col_sum[j] = 0
-                    for k in range(i, m):
-                        if grid[k][j] == 'W':
-                            break
-                        if grid[k][j] == 'E':
-                            col_sum[j] += 1
-                if grid[i][j] == '0':
+                    k = i
+                    while k < m and grid[k][j] != 'W':
+                        col_sum[j] += grid[k][j] == 'E'
+                        k += 1
+                if grid[i][j] =='0':
                     res = max(res, row_sum + col_sum[j])
-                
+        
         return res
