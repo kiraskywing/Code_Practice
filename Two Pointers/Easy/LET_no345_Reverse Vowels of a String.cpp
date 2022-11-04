d0c051061a84fbe9bcef48a2d678e865967808fb
Yi-Chang Lin <kiraskywing@gmail.com>
2022-11-04 09:19:15 -0500

    update 20221104

---
+++ Two Pointers/Easy/LET_no345_Reverse Vowels of a String.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            left = s.find_first_of("aeiouAEIOU", left);
+            right = s.find_last_of("aeiouAEIOU", right);
+            if (left < right) {
+                swap(s[left++], s[right--]);
+            }
+        }
+        
+        return s;
+    }
+};
