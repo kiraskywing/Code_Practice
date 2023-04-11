c7b921e5979d43fe6f94cfc482f308a83cecfd00
Yi-Chang Lin <kiraskywing@gmail.com>
2023-04-11 13:45:47 -0500

    update 20230411

---
+++ Two Pointers/Medium/LET_no2390_Removing Stars From a String.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        int i = 0;
+        for (int j = 0; j < s.size(); j++) {
+            if (s[j] == '*')
+                i--;
+            else 
+                s[i++] = s[j];
+        }
+
+        return s.substr(0, i);
+    }
+};
