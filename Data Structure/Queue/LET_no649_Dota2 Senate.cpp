bdbd9574e8a20af169a0410ab74034ed2fda6d68
Yi-Chang Lin <kiraskywing@gmail.com>
2023-05-04 08:33:33 +0800

    update 20230504

---
+++ Data Structure/Queue/LET_no649_Dota2 Senate.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        queue<int> qr, qd;
+        int n = senate.size();
+        for (int i = 0; i < n; i++) {
+            if (senate[i] == 'R')
+                qr.push(i);
+            else
+                qd.push(i);
+        }
+
+        while (!qr.empty() && !qd.empty()) {
+            int r = qr.front(), d = qd.front();
+            qr.pop(), qd.pop();
+            if (r < d)
+                qr.push(r + n);
+            else
+                qd.push(d + n);
+        }
+
+        return !qr.empty() ? "Radiant" : "Dire";
+
+    }
+};
