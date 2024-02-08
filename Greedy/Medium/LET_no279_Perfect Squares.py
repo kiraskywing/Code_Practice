fdececa836b6924628d4828aded960054cad115b
Yi-Chang Lin <kiraskywing@gmail.com>
2024-02-07 22:01:46 -0800

    update 20240207

--- Greedy/Medium/LET_no279_Perfect Squares.py
+++ Greedy/Medium/LET_no279_Perfect Squares.py
@@ -2,22 +2,23 @@ class Solution:
     def numSquares(self, n: int) -> int:
         visited = [False] * (n + 1)
         squares = []
-        for i in range(1, int(n ** 0.5) + 1):
-            squares.append(i * i)
-            visited[i] = True
+        for edge in range(1, int(n ** 0.5) + 1):
+            square = edge * edge
+            visited[square] = True
+            squares.append(square)
+
         queue = collections.deque(squares)
-        
         res = 1
         while queue:
             for _ in range(len(queue)):
                 cur = queue.popleft()
                 if cur == n:
                     return res
-                for num in squares:
-                    nxt = cur + num
+                for square in squares:
+                    nxt = cur + square
                     if nxt <= n and not visited[nxt]:
                         visited[nxt] = True
                         queue.append(nxt)
-                    elif nxt > n:
-                        break
             res += 1
+        
+        return 0
