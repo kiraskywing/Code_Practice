bf23f399505fb257d0212261be7de636a0851255
Yi-Chang Lin <kiraskywing@gmail.com>
2024-02-17 08:40:06 -0800

    update 20240216

---
+++ Data Structure/Heap/LET_no1642_Furthest Building You Can Reach.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        int n = heights.size();
+        for (int i = 0; i < n - 1; i++) {
+            int diff = heights[i + 1] - heights[i];
+            if (diff > 0)
+                pq.push(diff);
+
+            if (pq.size() > ladders) {
+                diff = pq.top();
+                pq.pop();
+                bricks -= diff;
+            }
+
+            if (bricks < 0)
+                return i;
+        }
+
+        return n - 1;
+    }
+};
