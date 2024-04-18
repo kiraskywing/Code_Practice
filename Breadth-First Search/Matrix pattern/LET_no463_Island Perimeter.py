c9aebb6bfd5d0c5462ed33666a47558af20482bd
Yi-Chang Lin <kiraskywing@gmail.com>
2024-04-17 22:26:50 -0700

    update 20240417

--- Breadth-First Search/Matrix pattern/LET_no463_Island Perimeter.py
+++ Breadth-First Search/Matrix pattern/LET_no463_Island Perimeter.py
@@ -1,5 +1,3 @@
-# The same as LintCode no1225. Island Perimeter
-
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         queue = collections.deque([])
@@ -27,3 +25,17 @@ class Solution:
                         grid[i2][j2] = -1
         
         return res
+    
+class Solution2:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        count = neighbor = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]:
+                    count += 1
+                    if i > 0 and grid[i - 1][j]:
+                        neighbor += 1
+                    if j > 0 and grid[i][j - 1]:
+                        neighbor += 1
+
+        return count * 4 - neighbor * 2
