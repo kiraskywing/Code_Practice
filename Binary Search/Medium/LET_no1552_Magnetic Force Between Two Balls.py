90af61b52fd1e2c1fd1433aff1156ff0c041ab18
Yi-Chang Lin <kiraskywing@gmail.com>
2024-06-19 21:30:32 -0700

    update 20240619

--- Binary Search/Medium/LET_no1552_Magnetic Force Between Two Balls.py
+++ Binary Search/Medium/LET_no1552_Magnetic Force Between Two Balls.py
@@ -1,26 +1,24 @@
 class Solution:
     def maxDistance(self, position: List[int], m: int) -> int:
-        self.n = len(position)
         position.sort()
-        
         left, right = 0, position[-1] - position[0]
         while left + 1 < right:
             mid = (left + right) // 2
-            if self.count(mid, position) >= m:
+            if self.helper(mid, position) >= m:
                 left = mid
             else:
                 right = mid
-        
-        if self.count(right, position) == m:
+
+        if self.helper(right, position) >= m:
             return right
-        
         return left
+
+    def helper(self, least_dist, pos):
+        res = 1
+        prev_pos = pos[0]
+        for i in range(1, len(pos)):
+            if pos[i] - prev_pos >= least_dist:
+                res += 1
+                prev_pos = pos[i]
         
-        
-    def count(self, d, position):
-        ans, cur = 1, position[0]
-        for i in range(1, self.n):
-            if position[i] - cur >= d:
-                ans += 1
-                cur = position[i]
-        return ans
+        return res
